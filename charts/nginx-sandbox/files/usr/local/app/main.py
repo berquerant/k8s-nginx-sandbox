3029c1a78b7a78e8d80275067e3c40dc2da11cc9
berquerant <sin@sin-pro.local>
2025-01-11 21:11:44 +0900

    Overwrite app main script instead of adding

--- charts/nginx-sandbox/files/usr/local/app/main.py
+++ charts/nginx-sandbox/files/usr/local/app/main.py
@@ -1,14 +1,4 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/health")
-def health():
-    return "OK"
-
 {{/*
 additional app configurations
 */}}
-{{- with .Values.app.conf.main }}
-{{ . }}
-{{- end}}
+{{- .Values.app.conf.main }}
